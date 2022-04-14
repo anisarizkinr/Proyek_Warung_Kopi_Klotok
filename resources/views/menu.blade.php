7114992aaff44937fc1ace03080c7409dfd02cda
Shintya77 <shintyarahma77@gmail.com>
2022-04-14 13:16:43 -0700

    edit footer dan menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -10,11 +10,11 @@
               <div class="card card-span mb-3 shadow-lg">
                 <div class="card-body py-0">
                   <div class="row justify-content-center">
-                    <div class="col-md-5 col-xl-7 col-xxl-8 g-0 order-0 order-md-1"><img class="img-fluid w-100 fit-cover h-100 rounded-top rounded-md-end rounded-md-top-0" src="assets/img/gallery/crispy-sandwiches.png" alt="..." /></div>
+                    <div class="col-md-5 col-xl-7 col-xxl-8 g-0 order-0 order-md-1"><img class="img-fluid w-100 fit-cover h-100 rounded-top rounded-md-end rounded-md-top-0" src="assets/img/gallery/kopi1.jpg" alt="..." /></div>
                     <div class="col-md-7 col-xl-5 col-xxl-4 p-4 p-lg-5">
-                      <h1 class="card-title mt-xl-5 mb-4">Best deals <span class="text-primary"> Crispy Sandwiches</span></h1>
-                      <p class="fs-1">Enjoy the large size of sandwiches. Complete your meal with the perfect slice of sandwiches.</p>
-                      <div class="d-grid bottom-0"><a class="btn btn-lg btn-primary mt-xl-6" href="#!">PROCEED TO ORDER<i class="fas fa-chevron-right ms-2"></i></a></div>
+                      <h1 class="card-title mt-xl-5 mb-4">Nikmati Menu Andalan<span class="text-primary"> Dengan Kopi Klotok </span></h1>
+                      <p class="fs-1">Nikmati Makanan dan Minumanmu dengan Suasana Pedesaan yang Asri</p>
+                      <div class="d-grid bottom-0"><a class="btn btn-lg btn-primary mt-xl-6" href="#!">ORDER NOW<i class="fas fa-chevron-right ms-2"></i></a></div>
                     </div>
                   </div>
                 </div>
@@ -46,44 +46,44 @@
           <div class="carousel-item active" data-bs-interval="10000">
             <div class="row h-100 align-items-center">
               <div class="col-sm-6 col-md-4 col-xl mb-5 h-100">
-                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/search-pizza.png" alt="..." />
+                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/kopi1.jpg" alt="..." />
                   <div class="card-body ps-0">
-                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">pizza</h5>
+                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Kopi Klotok</h5>
                   </div>
                 </div>
               </div>
               <div class="col-sm-6 col-md-4 col-xl mb-5 h-100">
-                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/burger.png" alt="..." />
+                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/pisang_goreng.jpg" alt="..." />
                   <div class="card-body ps-0">
-                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Burger</h5>
+                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Pisang Goreng</h5>
                   </div>
                 </div>
               </div>
               <div class="col-sm-6 col-md-4 col-xl mb-5 h-100">
-                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/noodles.png" alt="..." />
+                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/ayam_bakar_pedas.jpg" alt="..." />
                   <div class="card-body ps-0">
-                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Noodles</h5>
+                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Ayam Bakar Pedas</h5>
                   </div>
                 </div>
               </div>
               <div class="col-sm-6 col-md-4 col-xl mb-5 h-100">
-                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/sub-sandwich.png" alt="..." />
+                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/nasi_sayur_lodeh.jpg" alt="..." />
                   <div class="card-body ps-0">
-                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Sub-sandwiches</h5>
+                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Nasi Sayur Lodeh</h5>
                   </div>
                 </div>
               </div>
               <div class="col-sm-6 col-md-4 col-xl mb-5 h-100">
-                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/chowmein.png" alt="..." />
+                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/telur_dadar.png" alt="..." />
                   <div class="card-body ps-0">
-                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Chowmein</h5>
+                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Telur Dadar</h5>
                   </div>
                 </div>
               </div>
               <div class="col-sm-6 col-md-4 col-xl mb-5 h-100">
-                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/steak.png" alt="..." />
+                <div class="card card-span h-100 rounded-circle"><img class="img-fluid rounded-circle h-100" src="assets/img/gallery/ayam_bakar.jpg" alt="..." />
                   <div class="card-body ps-0">
-                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Steak</h5>
+                    <h5 class="text-center fw-bold text-1000 text-truncate mb-2">Ayam Bakar</h5>
                   </div>
                 </div>
               </div>
